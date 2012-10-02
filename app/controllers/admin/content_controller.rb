c5d5598f1fa8f42daa829d2c6213d9cbc56cb363
Pete Michaud <michaudp@gmail.com>
2012-10-02 15:43:12 -0500

    getting posts is more concise

--- app/controllers/admin/content_controller.rb
+++ app/controllers/admin/content_controller.rb
@@ -3,7 +3,7 @@ class Admin::ContentController < AdminController
   # LIST -- Shows a list of the content type [posts,pages,comments,messages,ads,etc...]
   def index
     # Set our limit, this should be dynamic or something.. drop box?
-    limit = 12;
+    limit = 12
 
     # if we are talking about a tag, get our posts from there
     #@tag = params[:tag] || ''
@@ -19,12 +19,8 @@ class Admin::ContentController < AdminController
     #  @posts = list_models category.posts, [], 'go_live desc' if params[:type] == 'posts'
     #end
 
-    #if !@tag.present? && !@category.present?
 
-    @content = list_models Post.posts, [], 'go_live desc' if params[:type] == 'posts'
-    @content = list_models Post.pages, [], 'go_live desc' if params[:type] == 'pages'
-
-    #end
+    @posts = PostDecorator.decorate(list_models Post.send(params[:type]), [], 'go_live desc')
 
     respond_to do |format|
       format.html # index.html.erb
